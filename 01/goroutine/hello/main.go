6d67f4c363739d8cf6fbe013570d3efcbdf59629
HyunminKo <hyunmin.ko4578@gmail.com>
2020-10-13 23:44:45 +0900

    01-hello

---
+++ 01/goroutine/hello/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fun(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func main() {
+	fun("direct call")
+
+	go fun("goroutine-1")
+
+	go func() {
+		fun("goroutine-2")
+	}()
+
+	fv := fun
+	go fv("goroutine-3")
+
+	fmt.Println("wait for goroutines")
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("done...")
+}
