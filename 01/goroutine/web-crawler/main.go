f0dd9d9cc06c917b725e56422eb9def9e2d99538
HyunminKo <hyunmin.ko4578@gmail.com>
2020-10-18 23:09:38 +0900

    01-webcrawler-concurrent

--- 01/goroutine/web-crawler/main.go
+++ 01/goroutine/web-crawler/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 	"time"
 
 	"golang.org/x/net/html"
 )
 
 var fetched map[string]bool
+
+type result struct {
+	url   string
+	urls  []string
+	err   error
+	depth int
+}
 
 func Crawl(url string, depth int) {
 	if depth < 0 {
@@ -31,10 +39,41 @@ func Crawl(url string, depth int) {
 	return
 }
 
+func ConcurrentCrawl(url string, depth int) {
+	ch := make(chan *result)
+
+	fetch := func(url string, depth int) {
+		urls, err := findLinks(url)
+		ch <- &result{url, urls, err, depth}
+	}
+
+	go fetch(url, depth)
+	fetched[url] = true
+
+	for fetching := 1; fetching > 0; fetching-- {
+		res := <-ch
+		if res.err != nil {
+			continue
+		}
+		fmt.Printf("found: %s\n", res.url)
+
+		if res.depth > 0 {
+			for _, u := range res.urls {
+				if !fetched[u] {
+					fetching++
+					go fetch(u, res.depth-1)
+					fetched[u] = true
+				}
+			}
+		}
+	}
+	close(ch)
+}
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	fetched = make(map[string]bool)
 	now := time.Now()
-	Crawl("http://andcloud.io", 2)
+	ConcurrentCrawl("http://andcloud.io", 2)
 	fmt.Println("time taken:", time.Since(now))
 }
 
